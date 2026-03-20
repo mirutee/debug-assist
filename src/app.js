1a18ad45c61a3ac4ea9f4880fdedd4670fa6790b
DevInsight <dev@devinsight.com>
2026-03-20 00:48:59 -0300

    feat: integrar Swagger UI em /docs

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 // src/app.js
 require("dotenv").config();
 const express = require("express");
-const app = express();
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const path = require("path");
 
+const app = express();
 app.use(express.json());
+
+const swaggerDocument = YAML.load(path.join(__dirname, "../swagger.yaml"));
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use("/health", require("./routes/health"));
 app.use("/v1/diagnosticos", require("./routes/diagnosticos"));
